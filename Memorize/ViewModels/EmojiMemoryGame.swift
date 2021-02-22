b9e25fc773f638459d97dc382aaee2df70ee6eb3
Smith <smith.mugalu@andela.com>
2021-02-22 16:06:42 +0300

    lesson one and lesson two

---
+++ Memorize/ViewModels/EmojiMemoryGame.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Smithers on 22/02/2021.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻","🎃","🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    //MARK: - Access to the models
+    var cards: Array<MemoryGame<String>.Card> {
+         model.cards
+    }
+    
+    //MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
+
---
+++ Memorize/Views/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Smithers on 22/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+    var body: some View {
+        HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    self.viewModel.choose(card: card)
+                }
+            }
+        }
+        .foregroundColor(.orange)
+        .padding()
+        .font(.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    var body: some View {
+        ZStack {
+            if card.isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(viewModel: EmojiMemoryGame())
+    }
+}
